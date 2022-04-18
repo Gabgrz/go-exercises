771ece9ddd729058fe090cb7c54fc69a797897c7
ggarozgonzalez <ggarozgonzalez@paypal.com>
2022-04-17 20:22:06 -0600

    add error handling

--- hello/go.mod
+++ hello/go.mod
@@ -1,3 +1,5 @@
 module github.com/Gabgrz/golang/hello
 
 go 1.17
+
+require github.com/Gabgrz/golang/greetings v0.0.0-20220418020733-7c3d4fea1f96
--- hello/hello.go
+++ hello/hello.go
@@ -2,11 +2,29 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/Gabgrz/golang/greetings"
 )
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	// Request a correct greeting message
 	message := greetings.Hello("Gabriel")
 	fmt.Println(message)
+
+	// Request an incorrect greeting message
+	message, err := greetings.Hello("")
+	// If an error was returned, print it to the console and
+	// exit the program.
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(message)
+
 }
